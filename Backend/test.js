e27b4200fec8afd02389e5b35404e987caac558a
ryansallen98 <115708378+ryansallen98@users.noreply.github.com>
2023-05-11 00:52:23 +0100

    updates

---
+++ Backend/test.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const jwt = require('jsonwebtoken');
+const jwa = require('jwa');
+const { decodeSubjectChain } = require('relay-jwt');
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 3000;;
+
+// Set up JSON body parsing middleware with the specified MIME types and maximum
+// request body size
+const jsonOptions = {
+    type: ["*/json"],
+    limit: "750kb",
+  }
+  app.use(express.json(jsonOptions));
+
+// Middleware to parse JSON request body
+app.use(bodyParser.json());
+
+async function newPostIPN (req, res) {
+  const ipAddress = req.connection.remoteAddress;
+  console.log(ipAddress, req.body, req.headers['x-forwarded-for']);
+  res.send("OK");
+}
+
+app.post("/ipn", newPostIPN);
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
